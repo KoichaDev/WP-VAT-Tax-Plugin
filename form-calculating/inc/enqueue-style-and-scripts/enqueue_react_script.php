b05bbbd14464b9e691734b8dd3953a8364c36f39
Khoi Hoang <khoii87@gmail.com>
2021-06-16 21:29:53 +0200

    Add: 📦 SASS dependency + enqueuing CSS files

--- form-calculating/inc/enqueue-style-and-scripts/enqueue_react_script.php
+++ form-calculating/inc/enqueue-style-and-scripts/enqueue_react_script.php
@@ -19,9 +19,12 @@ function script_defer($url) {
 function enqueue_react_script() {
     if (basename($_SERVER['REQUEST_URI']) === 'admin.php?page=calculate_tax_vat') {
         $file_to_js = dirname(plugin_dir_url(__DIR__), 1) . '/dist/index.js';
+        $file_to_css = dirname(plugin_dir_url(__DIR__), 1) . '/dist/index.css';
+
         $file_version = round(microtime(true), 0);
 
         // Load regular JavaScript with the React 
+        wp_enqueue_style('style-sheet', $file_to_css, [], $file_version, 'all');
         wp_enqueue_script('react-script', $file_to_js, [], $file_version, false);
     }
 
