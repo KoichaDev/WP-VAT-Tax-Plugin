559991871433dab43214076a8bd89f7916a289f7
Khoi Hoang <khoii87@gmail.com>
2021-06-23 00:22:43 +0200

    Fix: 🐛 Updating the ACF JSON locally
    Forgot to add this feature so it will be auto load the ACF json file of
    the fields

--- form-calculating/backend/acf-local-json/acf-local-json.php
+++ form-calculating/backend/acf-local-json/acf-local-json.php
@@ -2,12 +2,35 @@
 
 define('GS_FORM_CALCULATING_PLUGIN_DIR_PATH', untrailingslashit(plugin_dir_path(__FILE__)));
 
+/**
+ * Register the path to save the ACF json files so that they are version controlled.
+ * @param $paths The default relative path to the folder where ACF saves the files.
+ * @return string The new relative path to the folder where we are saving the files.
+ */
+
 function gs_acf_json_save_point($path) {
 
-    // Update path
+    // Retrievev the updated path where ACF is going to be saved
     $path = GS_FORM_CALCULATING_PLUGIN_DIR_PATH;
 
     // Return path
     return $path;
 }
 add_filter('acf/settings/save_json', 'gs_acf_json_save_point');
+
+
+
+/**
+ * Register the path to load the ACF json files so that they are version controlled.
+ * @param $paths The default relative path to the folder where ACF loading the files.
+ * @return string The new relative path to the folder where we are saving the files.
+ */
+
+function gs_acf_json_load_point($paths) {
+    // Remove original path
+    unset($paths[0]); // Append our new path
+    $paths[] = GS_FORM_CALCULATING_PLUGIN_DIR_PATH;
+    return $paths;
+}
+
+add_filter('acf/settings/load_json', 'gs_acf_json_load_point');
